3e7bfec7d5708c6f48e101f354b7919db3810dd4
Wolwer1ne <wolwerine@yandex.ru>
2023-12-01 10:50:24 +0300

    Add grades for task 3

--- src/index.js
+++ src/index.js
@@ -9,6 +9,17 @@ document.addEventListener('DOMContentLoaded', ()=>{
     standings.students = standings.students
         .sort((a, b) => b.totalScore - a.totalScore)
 
+    const previousStandings = standings.students.map(x=>{
+        return {
+            id: x.id,
+            totalScore: x.totalScore - x.scores[x.scores.length-1]
+        }
+    }).sort((a, b) => b.totalScore - a.totalScore)
+
+    for (let i = 0; i < standings.students.length; i++) {
+        const student = standings.students[i]
+        student.progress = previousStandings.findIndex(x=>x.id===student.id) - i
+    }
     var app = new Vue({
         el: '#studentsList',
         data: {
